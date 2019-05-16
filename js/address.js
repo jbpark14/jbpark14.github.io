4148f722b4589703bb176678dc145c39bd54080b
jbpark14 <49962677+jbpark14@users.noreply.github.com>
2019-05-16 10:13:36 -0600

    5.16

--- js/address.js
+++ js/address.js
@@ -6,13 +6,10 @@ let output = $.getElementById('output'),
 		city = $.getElementById('city'),
 		state = $.getElementById('state'),
 		zip = $.getElementById('zip');
-
-//Here I added an event listener to each input so that when you keyup on your keyboard, it executes the displayAddress function. But for people who are using touch screens, I left the button option there as well. 
 city.addEventListener('keyup', displayAddress );
 state.addEventListener('keyup', displayAddress );
 zip.addEventListener('keyup', displayAddress );
 
 function displayAddress(){
-	//I'm using the backtick syntax introduced in ES6. To insert values you use this syntax ${variableHere}
 	output.innerHTML = `${city.value}, ${state.value} ${zip.value}`
 }

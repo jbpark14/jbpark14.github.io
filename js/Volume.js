78a2ab579ae56a85ac4800ac465670296d3b4a82
jbpark14 <49962677+jbpark14@users.noreply.github.com>
2019-05-18 20:58:30 -0600

    5.18

--- js/Volume.js
+++ js/Volume.js
@@ -5,12 +5,6 @@ let date =
   today.getMonth() + 1 + " / " + today.getDate() + " / " + today.getFullYear();
 document.getElementById("currentdate").innerHTML = date;
 
-/* Design Table
-INPUT: Designate r as radius and h as height, receive input from HTML form.
-PROCESSING: Complete formula for finding the volume of a cylinder in inches cubed and then convert inches cubed to liters. 
-OUTPUT: Updates the output value in HTML and displays on screen. 
-*/
-
 // INPUT
 function math() {
   let r = document.getElementById("r").value;

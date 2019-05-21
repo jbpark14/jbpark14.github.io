afd0a628ef821fe81547edbdb9b137e885b6bb18
jbpark14 <49962677+jbpark14@users.noreply.github.com>
2019-05-21 10:58:31 -0600

    5.21

--- js/Volume.js
+++ js/Volume.js
@@ -1,16 +1,10 @@
-// Get Current Date
 let today = new Date(); // new Date object
-// now concatenate formatted output
 let date =
   today.getMonth() + 1 + " / " + today.getDate() + " / " + today.getFullYear();
 document.getElementById("currentdate").innerHTML = date;
-
-// INPUT
 function math() {
   let r = document.getElementById("r").value;
   let h = document.getElementById("h").value;
   let output = document.getElementById("output");
-// PROCESSING
-// OUTPUT
   output.value = Math.PI * Math.pow (r, 2) * h / 61.024;
 }

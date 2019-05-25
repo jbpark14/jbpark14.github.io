653136f39f3250aa385c30521b896098fa0d8999
jbpark14 <49962677+jbpark14@users.noreply.github.com>
2019-05-25 09:48:25 -0600

    Update volume.js

--- js/volume.js
+++ js/volume.js
@@ -1,19 +1,18 @@
 /* Defining Table
-INPUT:  Compute the volume of a cylinder in liters. Ask for the approriate inputs.Output the total volume to the screen.  Your program should handle real numbers (numbers with decimals)
-PROCESSING:  Use the formula to calculate volume  V=πr2h
-OUTPUT:  Display units */
+Compute the volume of a cylinder in liters. Ask for the appropriate inputs and output the total volume to the screen. Your program should correctly handle real numbers (numbers with decimals).
 
-function  calculateVolume(){
+INPUT: get height of the cylinder (h) and the radius (r) 
+PROCESSING: compute the volume of a cylinder using V=πr2h
+OUTPUT: Display volume in cubic units
+*/
 
+function getVolume() {
 	//INPUT
-	let r =parseFloat(document.getElementById
-					('r').value);
-let h =parseFloat(document.getElementById ('h').value);
-	
-	//PROCESSING
-let volume = Math.PI * (Math.pow (r,2)*h)
-	
+	let r = parseFloat(document.getElementById('r').value);
+	let h = parseFloat(document.getElementById('h').value);
+	//Processing
+	let volume = Math.PI * Math.pow(r, 2) * h;
+	let liters = volume / 61.024;
 	//OUTPUT
-	document.getElementById("output").innerHTML = "Your Volume Is: " + volume + " liters";
-	
+	document.getElementById('output').innerHTML = "The volume of the cylinder is " + liters.toFixed(2) + " liters";
 }

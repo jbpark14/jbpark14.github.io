a6775c53c3ac3f2915bf966d92f1c291046eeb20
jbpark14 <49962677+jbpark14@users.noreply.github.com>
2019-06-15 20:54:23 -0600

    6.15

--- js/integers.js
+++ js/integers.js
@@ -1,12 +1,14 @@
-//INPUT  get integer from user and set range of numbers allowed
-function oddSum() {
-	let n = parseInt(document.getElementById("n").value);
 
-//PROCESSING calculate the user number and find all odd numbers less than user number.
-	let sum = 0;
-	for(let i = 1; i <= n; i += 2) {
-			sum += i;
-	}
-	//OUTPUT display total number for user.
-document.getElementById("output").innerHTML = sum;
-}
+function Sum() {
+	//inputINPUT: Get any integer from the user
+	  let n = parseInt(document.getElementById("number").value); 
+	  //PROCESSING: Add up all of the odd numbers between 1- integer.
+	  let sum = 0;
+	  for (let i = 1; i <= n; i += 2) {
+		  sum += i
+	  }
+	  
+	  //OUTPUT: Display results to user. 
+	document.getElementById("text").innerHTML = "All odd integers add up to be: ";
+	document.getElementById("output").innerHTML = sum;
+	  }
